bb1eafd245fe17cb15be2d710b1e555ea8c99753
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-05-25 14:47:44 -0400

    - Successfully managed to add and delete row from the first section

--- AddingCellsTableViewTest/ViewController.swift
+++ AddingCellsTableViewTest/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UITableViewController {
     
     var numberOfSections = 1 {
         didSet {
-            tableView.reloadData()
+            
         }
     }
  //MARK:- Number of rows for each section
@@ -26,18 +26,15 @@ class ViewController: UITableViewController {
         didSet {
             if numberOfRowsInSection1 == 5 {
                 addNameButton.isEnabled = false
+            } else {
+                addNameButton.isEnabled = true
             }
-            tableView.reloadData()
-            //Note: First attempt at animation for tableview
-//            let indexPath = IndexPath(row: numberOfRowsInSection1, section: 0)
-//            tableView.beginUpdates()
-//            tableView.reloadRows(at: [indexPath], with: .fade)
-//            tableView.endUpdates()
+            print(numberOfRowsInSection1)
+//            tableView.reloadData()
         }
     }
     var numberOfRowsInSection2 = 1 {
         didSet {
-            tableView.reloadData()
         }
     }
     var numberOfRowsInSection3 = 1 {
@@ -62,11 +59,22 @@ class ViewController: UITableViewController {
         deleteNickNameButton.tintColor = UIColor.systemRed
     }
 //MARK:- Add Rows
+    private func addRows(numOfRows: Int, section: Int) {
+        let indexPath = IndexPath(row: numOfRows - 1, section: section)
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .right)
+        tableView.endUpdates()
+    }
     @IBAction func addRowsSection1(_ sender: UIButton) {
         numberOfRowsInSection1 += 1
+        addRows(numOfRows: numberOfRowsInSection1, section: 0)
     }
     @IBAction func addRowsSection2(_ sender: UIButton) {
         numberOfRowsInSection2 += 1
+        let indexPath = IndexPath(row: numberOfSections, section: 0)
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .right)
+        tableView.endUpdates()
     }
     @IBAction func addRowsSection3(_ sender: UIButton) {
         numberOfRowsInSection3 += 1
@@ -75,16 +83,31 @@ class ViewController: UITableViewController {
         numberOfRowsInSection4 += 1
     }
     //MARK:- Delete rows
+    private func deleteRows(row: Int, section: Int) {
+        let indexPath = IndexPath(row: row, section: section)
+        tableView.beginUpdates()
+        tableView.reloadRows(at: [indexPath], with: .fade)
+        tableView.endUpdates()
+    }
     @IBAction func deleteFirstNamePressed(_ sender: UIButton) {
+        deleteRows(row: 1, section: 0)
+        numberOfRowsInSection1 -= 1
+        tableView.reloadData()
     }
     @IBAction func deleteLastNamePressed(_ sender: UIButton) {
-        
+        deleteRows(row: 2, section: 0)
+        numberOfRowsInSection1 -= 1
+        tableView.reloadData()
     }
     @IBAction func deleteMiddleNamePressed(_ sender: UIButton) {
-        
+        deleteRows(row: 3, section: 0)
+        numberOfRowsInSection1 -= 1
+        tableView.reloadData()
     }
     @IBAction func deleteNickNamePressed(_ sender: UIButton) {
-        
+        deleteRows(row: 4, section: 0)
+        numberOfRowsInSection1 -= 1
+        tableView.reloadData()
     }
     
     @IBAction func addSectionButtonPressed(_ sender: UIButton) {
